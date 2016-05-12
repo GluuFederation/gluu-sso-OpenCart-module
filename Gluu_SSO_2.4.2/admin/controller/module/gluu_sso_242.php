9df2cb2068a0be5ee55434e83d5dd351906237d1
dollar007 <vlad.karapetyan.1988@mail.ru>
2016-05-12 11:45:29 +0400

    adding  controller, view, css and js files for module admin part

---
+++ Gluu_SSO_2.4.2/admin/controller/module/gluu_sso_242.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Module controller
+*/
+class ControllerModuleGluuSSO242 extends Controller
+{
+
+    /*
+     * Module installation function
+    */
+    public function install()
+    {       
+
+    }
+
+    /*
+     * Module loading page
+    */
+    public function index() {
+
+    }
+}
+
+?>
---
+++ Gluu_SSO_2.4.2/admin/view/javascript/gluu_sso_242/gluu_sso_242.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Vlad-Home on 5/12/2016.
+ */
