ab9721bf0f42ae4946fce711481e8e1140feae46
dollar007 <vlad.karapetyan.1988@mail.ru>
2016-05-12 11:52:46 +0400

    creating table in mysql

--- Gluu_SSO_2.4.2/admin/controller/module/gluu_sso_242.php
+++ Gluu_SSO_2.4.2/admin/controller/module/gluu_sso_242.php
@@ -10,8 +10,12 @@ class ControllerModuleGluuSSO242 extends Controller
      * Module installation function
     */
     public function install()
-    {       
-
+    {
+        $this->db->query("CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "gluu_table` (
+                            `gluu_action` varchar(255) NOT NULL,
+                            `gluu_value` longtext NOT NULL,
+                            UNIQUE(`gluu_action`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8");
     }
 
     /*
@@ -20,6 +24,8 @@ class ControllerModuleGluuSSO242 extends Controller
     public function index() {
 
     }
+
+
 }
 
 ?>
